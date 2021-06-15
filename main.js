6c764ee644bd9a374923d9972e37c1e24695af07
tamanaya876 <76041889+tamanaya876@users.noreply.github.com>
2021-06-15 14:47:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+video = "";
+
 function preload()
 {
-
+    //Creating video inside variable
+    video = createVideo("video.mp4");
+    //Hiding video
+    video.hide();
 }
 
 function setup()
@@ -9,7 +14,30 @@ function setup()
     canvas.position(485,220);
 }
 
+function start()
+{
+    //Inside variable putting ml5 function objectDetector inside it cocossd model is loaded and calling function modelLoaded
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+    //Setting the status
+    document.getElementById("status").innerHTML = "Status : Object Detection"
+}
+
+function modelLoaded()
+{
+    //Displaying Model Loaded in console
+    console.log("Model Loaded!");
+    //Marking the status true
+    status = true;
+    //Repeating the video
+    video.loop();
+    //Setting speed of video
+    video.speed(1);
+    //Setting volume of video
+    video.volume(0);
+}
+
 function draw()
 {
-
+    //Displaying video in canvas
+    image(video,0,0,300,300);
 }
