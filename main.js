757a214032a0c0dd5ed81bc3bf721bdcc93f1808
tamanaya876 <76041889+tamanaya876@users.noreply.github.com>
2021-06-17 14:54:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-video = "";
+objects = [];
+status = "";
 
 function preload()
 {
     //Creating video inside variable
-    video = createVideo("video.mp4");
+    video = createVideo("video2.mp4");
     //Hiding video
     video.hide();
 }
@@ -36,8 +37,39 @@ function modelLoaded()
     video.volume(0);
 }
 
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+
+    else
+    {
+        console.log(results);
+        objects = results;
+    }
+}
+
 function draw()
 {
     //Displaying video in canvas
     image(video,0,0,300,300);
+
+    if(status != "")
+    {
+        objectDetector.detect(video,gotResults);
+        for(i = 0;i<objects.length;i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detection";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects = " + objects.length;
+
+            fill('red');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            stroke('red');
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
